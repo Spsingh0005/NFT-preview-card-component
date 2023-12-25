49ed45bbaf92f10c960fafc5b30d8e01ca88c2b2
Robin <robinsingh199815@gmail.com>
2023-12-25 23:17:25 +0530

    Js added

---
+++ script.js
@@ -0,0 +1,7 @@
+var box = document.getElementsByClassName('.eye');
+console.log(box);
+
+function showEye() {
+    document.getElementsByClassName('.eye').classList.add("show");
+    // console.log(box);
+}
